4125b63179fb337f885360a44490bd5ae022fb1a
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2024-06-26 15:08:03 +0300

    feat(group): add Pydantic models for group management

--- app/domain/entities/group.py
+++ app/domain/entities/group.py
@@ -1,1 +1,24 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from datetime import datetime
 
+
+class GroupBase(BaseModel):
+    name: str = Field(max_length=52)
+
+
+class GroupCreate(GroupBase):
+    pass
+
+
+class GroupUpdate(GroupBase):
+    pass
+
+
+class GroupUpdatePartial(BaseModel):
+    name: Optional[str] = Field(None, max_length=52)
+
+
+class Group(GroupBase):
+    id: int
+    created_at: datetime
