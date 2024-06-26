54074481c3668abfc06701898ae38c8d985961b6
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2024-06-26 11:53:55 +0300

    feat(settings): add database settings and connection URL

---
+++ app/core/config.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).parent.parent
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    db_echo: bool = False
+    # db_echo: bool = True
+    SETTINGS_DB_HOST: str
+    SETTINGS_DB_PORT: int
+    SETTINGS_DB_NAME: str
+    SETTINGS_DB_USER: str
+    SETTINGS_DB_PASSWORD: str
+
+    def get_db_url(self):
+        return f"postgresql+asyncpg://{self.SETTINGS_DB_USER}:{self.SETTINGS_DB_PASSWORD}@{self.SETTINGS_DB_HOST}:{self.SETTINGS_DB_PORT}/{self.SETTINGS_DB_NAME}"
+
+
+settings = Settings()
