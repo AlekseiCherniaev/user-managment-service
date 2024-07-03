fb6eaaadd05d4d3a7354a6abc066ccefc991b4fd
AlekseyCherniaev <cherniaevaleksey2304@gmail.com>
2024-07-04 01:13:03 +0300

    fix(user_repo): add pydantic schemas to validation

--- app/adapters/controllers/users.py
+++ app/adapters/controllers/users.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.dependencies.db_helper import db_helper
-from app.domain.entities.user import UserCreate, UserUpdate
+from app.domain.entities.user import UserCreate, UserUpdate, User
 from app.repositories.user_repo import UserRepo
 
 user_router = APIRouter(
@@ -14,29 +14,29 @@ user_router = APIRouter(
 
 @user_router.get("/")
 async def get(user_id: UUID, user_repo: UserRepo = Depends(),
-              session: AsyncSession = Depends(db_helper.session_dependency)):
+              session: AsyncSession = Depends(db_helper.session_dependency)) -> User:
     return await user_repo.get_user(user_id, session)
 
 
 @user_router.get("/all/")
 async def get_all(user_repo: UserRepo = Depends(),
-                  session: AsyncSession = Depends(db_helper.session_dependency)):
+                  session: AsyncSession = Depends(db_helper.session_dependency)) -> list[User]:
     return await user_repo.get_all_users(session)
 
 
 @user_router.post("/create/")
 async def create(user_in: UserCreate, user_repo: UserRepo = Depends(),
-                 session: AsyncSession = Depends(db_helper.session_dependency)):
+                 session: AsyncSession = Depends(db_helper.session_dependency)) -> User:
     return await user_repo.create_user(user_in, session)
 
 
 @user_router.patch("/update/")
 async def update(user: UserUpdate, user_id: UUID, user_repo: UserRepo = Depends(),
-                 session: AsyncSession = Depends(db_helper.session_dependency)):
+                 session: AsyncSession = Depends(db_helper.session_dependency)) -> User:
     return await user_repo.update_user(user_id, user, session)
 
 
 @user_router.delete("/delete/")
 async def delete(user_id: UUID, user_repo: UserRepo = Depends(),
-                 session: AsyncSession = Depends(db_helper.session_dependency)):
+                 session: AsyncSession = Depends(db_helper.session_dependency)) -> None:
     return await user_repo.delete_user(user_id, session)
