82f58ac99f90a6bed809e727d093e45ccf185a07
AlekseyCherniaev <cherniaevaleksey2304@gmail.com>
2024-07-06 01:18:34 +0300

    feat(reset-password): add reset-password route (not implemented)

--- app/adapters/controllers/auth.py
+++ app/adapters/controllers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, Response
+from pydantic import EmailStr
 
 from app.adapters.controllers.users import create
 from app.dependencies.dependencies import validate_auth_user, get_current_auth_user_for_refresh, http_bearer
@@ -16,26 +17,38 @@ auth_router = APIRouter(
 @auth_router.post("/login/")
 async def login(
         user: User = Depends(validate_auth_user),
-        user_repository: UserRepo = Depends(), ) -> Token:
+        user_repository: UserRepo = Depends(),
+) -> Token:
     return await user_repository.login_user(user=user)
 
 
-@auth_router.post("/refresh-token/", response_model=Token, response_model_exclude_none=True, )
+@auth_router.post("/refresh-token/", response_model=Token, response_model_exclude_none=True)
 async def auth_refresh_jwt(
         user: User = Depends(get_current_auth_user_for_refresh),
-        user_repository: UserRepo = Depends()) -> Token:
+        user_repository: UserRepo = Depends(),
+) -> Token:
     return await user_repository.refresh_jwt(user=user)
 
 
 @auth_router.post("/signup")
 async def signup(
         user: User = Depends(create),
-        user_repository: UserRepo = Depends()) -> Token:
+        user_repository: UserRepo = Depends(),
+) -> Token:
     return await user_repository.signup_user(user=user)
 
 
 @auth_router.get("/logout/")
 async def logout(
         response: Response,
-        user_repository: UserRepo = Depends()) -> None:
+        user_repository: UserRepo = Depends(),
+) -> None:
     return await user_repository.logout_user(response=response)
+
+
+@auth_router.get("/reset-password")
+async def reset_password(
+        email: EmailStr,
+        user_repository: UserRepo = Depends(),
+) -> None:
+    return await user_repository.reset_password(email=email)
--- app/use_cases/auth_usecases.py
+++ app/use_cases/auth_usecases.py
@@ -1,7 +1,7 @@
 from fastapi import Response
+from pydantic import EmailStr
 
 from app.adapters.utils import create_access_token, create_refresh_token
-from app.config.exceptions import UserNotFoundException, WrongPasswordException
 from app.config.logger_config import logger
 from app.domain.entities.token import Token
 from app.domain.models.user import User
@@ -14,23 +14,9 @@ class AuthUseCases:
                          refresh_token=create_refresh_token(user),
                          token_type="Bearer",
                          )
-        except UserNotFoundException as e:
-            logger.error(f"User not found: {str(e)}")
-            raise e
-        except WrongPasswordException as e:
-            logger.error(f"Wrong password: {str(e)}")
-            raise e
-        except Exception as e:
-            logger.error(f"Error logging in user: {str(e)}")
 
-    async def refresh_jwt(self, user: User) -> Token:
-        try:
-            access_token = create_access_token(user)
-            return Token(
-                access_token=access_token,
-            )
         except Exception as e:
-            logger.error(f"Error refreshing JWT: {str(e)}")
+            logger.error(f"Error logining in: {str(e)}")
 
     async def signup(self, user: User) -> Token:
         try:
@@ -38,14 +24,26 @@ class AuthUseCases:
                          refresh_token=create_refresh_token(user),
                          token_type="Bearer",
                          )
-        except UserNotFoundException as e:
-            logger.error(f"User not found: {str(e)}")
-            raise e
+
         except Exception as e:
             logger.error(f"Error signing up: {str(e)}")
+
+    async def refresh_jwt(self, user: User) -> Token:
+        try:
+            access_token = create_access_token(user=user)
+            return Token(
+                access_token=access_token,
+            )
+
+        except Exception as e:
+            logger.error(f"Error refreshing JWT: {str(e)}")
 
     async def logout(self, response: Response) -> None:
 
         # TODO: Implement logout with adding into Blacklist
         response.delete_cookie("access_token")
         response.delete_cookie("refresh_token")
+
+    async def reset_password(self, email: EmailStr) -> None:
+        # # TODO: Implement reset password with publishing message to RabbitMQ
+        pass
