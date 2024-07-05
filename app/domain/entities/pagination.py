b5d29fd5eb42cf4f8298faf65f46d9950cd7a8e8
AlekseyCherniaev <cherniaevaleksey2304@gmail.com>
2024-07-06 00:18:47 +0300

    feat(pagination): add PaginationInfo model

---
+++ app/domain/entities/pagination.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class Order(Enum):
+    ASC = "ASC"
+    DESC = "DESC"
+
+
+class PaginationInfo(BaseModel):
+    page: int = Field(1, ge=1)
+    limit: int = Field(30, ge=1)
+
+    filter_by_name: str | None = None
+
+    sort_by: str | None = None
+
+    order_by: Order = "DESC"
